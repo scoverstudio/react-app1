7ddb627166e5e70820f14a48d0631e40acf0e9f0
Your Name <you@example.com>
2021-12-15 19:15:56 +0100

    add columns nad inputs

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,5 +1,6 @@
 import styles from './Column.module.scss'
-
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
 const Column = props => {
     return (
         <article className={styles.column}>
@@ -7,6 +8,10 @@ const Column = props => {
                 <span className={styles.icon + ' fa fa-' + props.icon} />
                 {props.title}
             </h2>
+            <ul className={styles.cards}>
+                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            </ul>
+            <CardForm action={props.addCard} />
         </article>
     );
 };
